9fdb4ea42c83d8ca8ac6db17d9e1edf03ee5082e
Brandon Simmons <brandon.m.simmons@gmail.com>
2022-09-14 12:01:24 -0400

    add some benchmarks

---
+++ benchmarks/Benchmarks.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE OverloadedStrings, BangPatterns #-}
+module Main where
+
+import Codec.Compression.LibDeflate.GZip
+import Foreign
+import Gauge.Main
+
+main :: IO ()
+main =
+  defaultMain
+    [ bgroup "libdeflate"
+        [ bench "alloc free compressor 1" $
+            whnfIO $ do
+              libdeflate_alloc_compressor 1 >>= libdeflate_free_compressor
+        , bench "alloc free compressor 12" $
+            whnfIO $ do
+              libdeflate_alloc_compressor 12 >>= libdeflate_free_compressor
+
+        , bgroup "level 1" [
+              bench "gzipCompress 1k" $
+                whnf (\p -> gzipCompress p 1 1000) zipPayload1k
+            , bench "gzipCompress 100k" $
+                whnf (\p -> gzipCompress p 1 100000) zipPayload100k
+            , bench "gzipCompress 10000k" $
+                whnf (\p -> gzipCompress p 1 10000000) zipPayload10000k
+            ]
+
+        , bgroup "level 6" [
+              bench "gzipCompress 1k" $
+                whnf (\p -> gzipCompress p 6 1000) zipPayload1k
+            , bench "gzipCompress 100k" $
+                whnf (\p -> gzipCompress p 6 100000) zipPayload100k
+            , bench "gzipCompress 10000k" $
+                whnf (\p -> gzipCompress p 6 10000000) zipPayload10000k
+            ]
+
+        , bgroup "level 12" [
+              bench "gzipCompress 1k" $
+                whnf (\p -> gzipCompress p 12 1000) zipPayload1k
+            , bench "gzipCompress 100k" $
+                whnf (\p -> gzipCompress p 12 100000) zipPayload100k
+            , bench "gzipCompress 10000k" $
+                whnf (\p -> gzipCompress p 12 10000000) zipPayload10000k
+            ]
+        ],
+      bgroup "misc"
+        [ -- bump allocated, so pretty cheap:
+          bench "mallocForeignPtrBytes 100" $ whnfAppIO mallocForeignPtrBytes 100,
+          bench "mallocForeignPtrBytes 10000" $ whnfAppIO mallocForeignPtrBytes 10000,
+          bench "mallocForeignPtrBytes 1000000" $ whnfAppIO mallocForeignPtrBytes 1000000
+        ]
+    ]
+  where !zipPayload100 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbccccc01234567890123456789"
+        !zipPayload1k = mconcat $ replicate 10 zipPayload100
+        !zipPayload100k = mconcat $ replicate 100 zipPayload1k
+        !zipPayload10000k = mconcat $ replicate 100 zipPayload100k
+
+-- TODO toStrict
--- src/Codec/Compression/LibDeflate/GZip.hsc
+++ src/Codec/Compression/LibDeflate/GZip.hsc
@@ -1,8 +1,13 @@
 {-# LANGUAGE PatternSynonyms, ViewPatterns #-}
 module Codec.Compression.LibDeflate.GZip (
-    LibDeflateCompressor,
     gzipCompress_reuse,
-    gzipCompress
+    gzipCompress,
+    -- * Low-level API
+    -- | See libdeflate.h for docs
+    LibDeflateCompressor,
+    libdeflate_free_compressor,
+    libdeflate_alloc_compressor,
+    libdeflate_gzip_compress
   ) where
 
 import Foreign
@@ -36,13 +41,15 @@ gzipCompress :: BS.ByteString -> Int -> Int -> Maybe BS.ByteString
 gzipCompress (BS.PS inputFpDirty off inputLen) compressionLevel maxCompressedSizeBytes = unsafePerformIO $ do
     -- NOTE: `PS` is a compatibility pattern synonym on bytestring <0.11
     let inputFp = inputFpDirty `plusForeignPtr` off
+    outputFp <- mallocForeignPtrBytes maxCompressedSizeBytes
     bracket
-    -- NOTE: this takes ~60us; these could be reused in threadsafe pool impl
+    -- NOTE: this takes ~3us to 13us depending on compression level; these
+    -- could be reused in threadsafe pool impl, but probably not warranted
       (libdeflate_alloc_compressor $ fromIntegral compressionLevel)
       libdeflate_free_compressor 
        $ \compressor -> do
+           -- TODO conditional unsafeWithForeignPtr
             withForeignPtr inputFp $ \inputPtr -> do
-                outputFp <- mallocForeignPtrBytes maxCompressedSizeBytes
                 compressedSizeBytes <- withForeignPtr outputFp $ \outputPtr -> do
                     libdeflate_gzip_compress compressor 
                       inputPtr (fromIntegral inputLen) 
@@ -50,7 +57,7 @@ gzipCompress (BS.PS inputFpDirty off inputLen) compressionLevel maxCompressedSiz
                 return $
                      BS.PS outputFp 0 (fromIntegral compressedSizeBytes) <$ guard (compressedSizeBytes > 0)
 
--- testing:
+-- XXX testing:
 gzipCompress_reuse :: BS.ByteString -> Int -> Maybe BS.ByteString
 {-# NOINLINE gzipCompress_reuse #-}
 gzipCompress_reuse (BS.PS inputFpDirty off inputLen) maxCompressedSizeBytes = unsafePerformIO $ do
